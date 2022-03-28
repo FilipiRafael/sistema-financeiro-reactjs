92dbcb4b408d97700bea1b0945dbe855bc0ff5b3
Filipi Rafael <filipirafael@MacBook-Air-de-Filipi.local>
2022-03-28 11:02:28 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item"
+
+export const items: Item[] = [
+    { date: new Date(2022, 2, 28), category: "food", title: "McDonalds", value: 32.12 },
+    { date: new Date(2022, 2, 28), category: "food", title: "Burguer King", value: 28.20 },
+    { date: new Date(2022, 3, 18), category: "rent", title: "Aluguel Apt", value: 1500.10 },
+    { date: new Date(2022, 2, 28), category: "salary", title: "Salary ACME", value: 2500.00 },
+];
